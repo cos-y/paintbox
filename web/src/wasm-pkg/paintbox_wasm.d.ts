376fd49f8be2edfafdc9fa1dc64669824ba16188
cosy <cos-y@qq.com>
2026-07-16 09:37:26 +0800

    [claude] remove wasm-pkg/.gitignore for vercel build

---
+++ web/src/wasm-pkg/paintbox_wasm.d.ts
@@ -0,0 +1,52 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export function color_diff(rgb_a: number, rgb_b: number): number;
+
+export function find_direct_equivalences(index: number): any;
+
+export function init_searcher(blob: Uint8Array, equiv_blob: Uint8Array): void;
+
+export function list_paints(): any;
+
+export function search(rgb: number, max_mix: number, limit: number, filter: any): any;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly find_direct_equivalences: (a: number) => [number, number, number];
+    readonly init_searcher: (a: number, b: number, c: number, d: number) => [number, number];
+    readonly list_paints: () => [number, number, number];
+    readonly search: (a: number, b: number, c: number, d: any) => [number, number, number];
+    readonly color_diff: (a: number, b: number) => number;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_exn_store: (a: number) => void;
+    readonly __externref_table_alloc: () => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __externref_table_dealloc: (a: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ web/src/wasm-pkg/paintbox_wasm_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const find_direct_equivalences: (a: number) => [number, number, number];
+export const init_searcher: (a: number, b: number, c: number, d: number) => [number, number];
+export const list_paints: () => [number, number, number];
+export const search: (a: number, b: number, c: number, d: any) => [number, number, number];
+export const color_diff: (a: number, b: number) => number;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
