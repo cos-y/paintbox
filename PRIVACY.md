ec67187a86ab8cde23766d12574252b2190548f3
cosy <cos-y@qq.com>
2026-08-04 15:31:59 +0800

    package name

---
+++ PRIVACY.md
@@ -0,0 +1,1 @@
+This application runs locally using wasm, does not rely on any online services, and does not collect user data.
--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -15,10 +15,10 @@ val tauriProperties = Properties().apply {
 
 android {
     compileSdk = 36
-    namespace = "com.paintbox.app"
+    namespace = "com.cosy.paintbox"
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
-        applicationId = "com.paintbox.app"
+        applicationId = "com.cosy.paintbox"
         minSdk = 24
         targetSdk = 36
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
